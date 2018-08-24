5bb84769cea258878cb4568502c9ff4a023b81d9
DATBOI <alexsimons9999@gmail.com>
2018-08-24 18:50:10 -0500

    test commit

--- src/main/java/io/acari/VariableSwegTargetProxy.java
+++ src/main/java/io/acari/VariableSwegTargetProxy.java
@@ -6,6 +6,7 @@ public class VariableSwegTargetProxy {
 
     public static void main(String... args) {
         try {
+            System.out.println();
             new SwegServer(Integer.parseInt(args[0]));
         } catch (IOException e) {
             e.printStackTrace();
